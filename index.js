61edd01d817da44f8efe98bc75c3c8726fea3f7a
nedoratic <nedoratic@gmail.com>
2024-02-06 20:48:51 +0100

    basic dom elements variables created

---
+++ index.js
@@ -0,0 +1,7 @@
+// DOM Elements Variables
+const playlistSongs = document.getElementById('playlist-songs');
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+const shuffleButton = document.getElementById('shuffle');
