9174f83f9cab9974103e438fcdc256c1f86cae6c
nedoratic <nedoratic@gmail.com>
2024-02-09 19:18:59 +0100

    sort playlist functionality added

--- index.js
+++ index.js
@@ -124,3 +124,17 @@ const renderSongs = (array) => {
 		.join('');
 	playlistSongs.innerHTML = songsHTML;
 };
+
+userData?.songs.sort((a, b) => {
+	if (a.title < b.title) {
+		return -1;
+	}
+
+	if (a.title > b.title) {
+		return 1;
+	}
+
+	return 0;
+});
+
+renderSongs(userData?.songs);
