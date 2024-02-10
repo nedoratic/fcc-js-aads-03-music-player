f5262de70b44b49c705c40e32eaea084c9af3603
nedoratic <nedoratic@gmail.com>
2024-02-10 22:40:02 +0100

    highlightCurrentSong function added

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ const playSong = (id) => {
 	}
 	userData.currentSong = song;
 	playButton.classList.add('playing');
+	highlightCurrentSong();
 	audio.play();
 };
 
@@ -126,6 +127,15 @@ const playPreviousSong = () => {
 		const previousSong = userData?.songs[currentSongIndex - 1];
 		playSong(previousSong.id);
 	}
+};
+
+const highlightCurrentSong = () => {
+	const playlistSongElements = document.querySelectorAll('.playlist-song');
+	const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`);
+	playlistSongElements.forEach((songEl) => {
+		songEl.removeAttribute('aria-current');
+	});
+	if (songToHighlight) songToHighlight.setAttribute('aria-current', 'true');
 };
 
 const renderSongs = (array) => {
