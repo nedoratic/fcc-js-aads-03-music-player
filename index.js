cbcd4633880ceee86f40e042187417a494fd9d19
nedoratic <nedoratic@gmail.com>
2024-02-13 22:02:38 +0100

    smooth transition between songs functionality added

--- index.js
+++ index.js
@@ -154,6 +154,7 @@ const deleteSong = (id) => {
 	if (userData?.songs.length === 0) {
 		const resetButton = document.createElement('button');
 		const resetText = document.createTextNode('Reset Playlist');
+
 		resetButton.id = 'reset';
 		resetButton.ariaLabel = 'Reset playlist';
 		resetButton.appendChild(resetText);
@@ -200,7 +201,7 @@ const renderSongs = (array) => {
 						<path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
 					</button>
 				</li>
-      		`;
+			`;
 		})
 		.join('');
 	playlistSongs.innerHTML = songsHTML;
@@ -228,6 +229,21 @@ nextButton.addEventListener('click', playNextSong);
 previousButton.addEventListener('click', playPreviousSong);
 
 shuffleButton.addEventListener('click', shuffle);
+
+audio.addEventListener('ended', () => {
+	const currentSongIndex = getCurrentSongIndex();
+	const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
+	if (nextSongExists) {
+		playNextSong();
+	} else {
+		userData.currentSong = null;
+		userData.songCurrentTime = 0;
+		pauseSong();
+		setPlayerDisplay();
+		highlightCurrentSong();
+		setPlayButtonAccessibleText();
+	}
+});
 
 userData?.songs.sort((a, b) => {
 	if (a.title < b.title) {
